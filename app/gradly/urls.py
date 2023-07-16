7ca4268dad18f79a7dca0aa5e1b048c9d1577e43
Vijay Singh <vijaysingh997022@gmail.com>
2023-07-16 21:17:52 +0530

    frontend

--- app/gradly/urls.py
+++ app/gradly/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        "api/docs",
+        "api/docs/",
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
