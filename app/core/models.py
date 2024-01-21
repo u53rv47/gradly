92eb69e8c1403123f4806c78f0caac275b4586c7
Vijay Singh <vijaysingh997022@gmail.com>
2024-01-21 14:06:57 +0530

    Initial deployment

--- app/core/models.py
+++ app/core/models.py
@@ -113,7 +113,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255)
 
-    apikey = models.CharField(max_length=32, null=True)
+    apikey = models.CharField(max_length=64, null=True)
     ai_models = models.ManyToManyField("AIModel", through="UserAIModel")
 
     first_name = models.CharField(max_length=255)
